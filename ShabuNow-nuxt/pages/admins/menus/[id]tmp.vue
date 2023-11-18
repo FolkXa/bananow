f499265270fda46b297ba483b22b825fabb6db67
FolkXa <jirakit11619@gmail.com>
2023-11-18 14:11:28 +0700

    add some feature and bug fix

--- ShabuNow-nuxt/pages/admins/menus/[id]tmp.vue
+++ ShabuNow-nuxt/pages/admins/menus/[id]tmp.vue
@@ -17,9 +17,9 @@
             </div>
             <div class="bg-cover-overlay"></div>
           </div>
-          <p class="mt-2">Name*</p>
+
           <InputField
-              class="mt-0"
+              class="mt-4"
               placeholder="ชื่อเมนู"
               type="text"
               name="name"
@@ -27,35 +27,33 @@
               @input="checkName"
           />
           <span v-if="errors.name" class="text-red-500">{{
-              errors.name[0]
+              errors.name
             }}</span>
-          <p class="mt-2">Price* (baht)</p>
           <InputField
-              class="mt-0"
+              class="mt-4"
               placeholder="ราคา (THB)"
               type="number"
               name="price"
               v-model="menu.price"
               @input="checkPrice"
           />
-          <p class="mt-2">Status*</p>
           <select v-model="menu.status"
-                  class="mt-0 text-base focus:ring-red-600 focus:border-red-600 focus:ring-1 focus:outline-none p-2.5 block bg-white border border-slate-300 rounded-md w-full py-3 pl-9 pr-3"
+              class="mt-4 text-base focus:ring-red-600 focus:border-red-600 focus:ring-1 focus:outline-none p-2.5 block bg-white border border-slate-300 rounded-md w-full py-3 pl-9 pr-3"
           >
             <option v-for="option in options" :value="option">
               {{ option }}
             </option>
           </select>
-          <p class="mt-2">Description*</p>
+
           <textarea
               row="2"
-              class="mt-0 text-base block p-2.5 w-full bg-white border border-slate-300 rounded-lg focus:ring-red-600 focus:border-red-600 focus:ring-1 focus:outline-none"
+              class="mt-4 text-base block p-2.5 w-full bg-white border border-slate-300 rounded-lg focus:ring-red-600 focus:border-red-600 focus:ring-1 focus:outline-none"
               placeholder="คำอธิบายเพิ่มเติม"
               v-model="menu.description"
               @input="checkDescription(menu.description)"
           ></textarea>
           <span v-if="errors.description" class="text-red-500">{{
-              errors.description[0]
+              errors.description
             }}</span>
           <div class="mx-auto my-8">
             <label
@@ -64,19 +62,12 @@
             >
             <input
                 id="example1"
-                accept="image/*"
                 type="file"
                 class="mt-2 block w-full text-sm file:mr-4 file:rounded-md file:border-0 file:bg-slate-700 file:py-2 file:px-4 file:text-sm file:font-semibold file:text-white hover:file:bg-slate-900 focus:outline-none disabled:pointer-events-none disabled:opacity-60"
-                @change="onChange"
             />
-            <div id="preview" class="w-1/2 mt-2">
-              <img class="rounded-xl object-cover h-[256px] w-[256px]" v-if="item.imageUrl" :src="item.imageUrl" alt="" />
-            </div>
           </div>
-          <span v-if="message" class="text-red-500">{{
-              message
-            }}</span>
-          <Button class="mt-4 w-full" type="submit">
+          <hr class="mt-4">
+          <Button class="mt-4 w-full">
             <slot name="button">ยืนยัน</slot>
           </Button>
         </div>
@@ -84,28 +75,26 @@
     </form>
   </MainContainer>
 </template>
+
 <script setup lang="js">
+import {useRoute} from "vue-router";
+import {useAuthStore} from "~/stores/auth";
+import {formToJSON} from "axios";
 
-import axios from "axios";
+// function onFileChanged(event) {
+//   console.log(event)
+// }
 
-const route = useRoute();
 const auth = useAuthStore();
 if (auth.getUser.role !== 'admin') {
   navigateTo('/');
 }
+const route = useRoute();
+
 const response = await $fetch(`http://localhost/api/menu/${route.params.id}`)
 const menu = reactive(response);
-const item = reactive( {
-  imageUrl : menu.imgPath
-})
 const options = ['available', 'outofstock']
 
-
-function onChange(e) {
-  const file = e.target.files[0]
-  menu.imgPath = file;
-  item.imageUrl = URL.createObjectURL(file)
-}
 function checkName() {
   if (menu.name.length > 20) {
     menu.name = menu.name.substring(0, 20)
@@ -124,63 +113,39 @@ function checkDescription() {
     menu.description = menu.description.substring(0, 30)
   }
 }
-const errors = ref([]);
-const message = ref();
+const errors = reactive({
+  name: null,
+  description: null
+})
 async function onSubmit() {
-  try {
-    // const response = await $fetch(`http://localhost/api/menu/store`, {
-    //   method : 'POST',
-    //   body : { ...menu }
-    // });
-    // console.log(response)
-    //
-    await axios.post(`http://localhost/api/menu/update/${route.params.id}`, menu, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    }).then(response => {
-      console.log(response)
-      navigateTo('/admins/editMenu')
-    }).catch(error => {
-      throw error
-    })
-  } catch (error) {
-    console.log('error : ' + error)
-    if (error.data.errors) {
-      errors.value = error.data.errors
-    } else {
-      message.value = error.data.message
-    }
+  if (menu.name.length === 0) {
+    errors.name = 'ห้ามว่าง';
+  } else {
+    errors.name = null;
   }
-
-  // if (menu.name.length === 0) {
-  //   errors.name = 'ห้ามว่าง';
-  // } else {
-  //   errors.name = null;
-  // }
-  // if (menu.description.length === 0) {
-  //   errors.description = "ห้ามว่าง";
-  // } else {
-  //   errors.description = null;
-  // }
-  // const name = menu.name;
-  // const price = menu.price;
-  // const imgPath = menu.imgPath;
-  // const description = menu.description;
-  // const status = menu.status;
-  // if (errors.name === null && errors.description === null) {
-  //   const response = await $fetch(`http://localhost/api/menu/store`, {
-  //     method : 'POST',
-  //     body : { ...menu }
-  //   });
-  //   console.log(response);
-  // }
+  if (menu.description.length === 0) {
+    errors.description = "ห้ามว่าง";
+  } else {
+    errors.description = null;
+  }
+  const name = menu.name;
+  const price = menu.price;
+  const imgPath = menu.imgPath;
+  const description = menu.description;
+  const status = menu.status;
+  if (errors.name === null && errors.description === null) {
+    const response = await $fetch(`http://localhost/api/menu/update/${route.params.id}`, {
+      method : 'POST',
+      body : { name, price, imgPath, description, status }
+    })
+    navigateTo('/admins/editMenu')
+  }
 }
 </script>
 <style>
 .bg-cover-category {
   position: relative;
-  background-image: url('~/assets/img/cover1.jpg');
+  background-image: url('../../../assets/img/cover1.jpg');
   background-position: center;
 }
 .bg-cover-overlay {
