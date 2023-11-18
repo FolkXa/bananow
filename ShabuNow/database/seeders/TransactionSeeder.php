d973dc1d6f42e0df3efa21e4b62d6858869a5fb3
FolkXa <jirakit11619@gmail.com>
2023-11-18 23:52:56 +0700

    add transaction feature

--- ShabuNow/database/seeders/TransactionSeeder.php
+++ ShabuNow/database/seeders/TransactionSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Http\Controllers\Api\TransactionContoller;
+use App\Models\Order;
 use App\Models\Transaction;
+use App\Models\User;
 use Database\Factories\TransactionFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +17,25 @@ class TransactionSeeder extends Seeder
      */
     public function run(): void
     {
-        $transaction = Transaction::factory()->count(20)->create();
+        $orders = Order::where('status', '!=', 'ordering')->get();
+        $users = User::where('role', 'staff')->get();
+        $users_count = $users->count();
+        $status = ['pending','in_queue','ready','done','rejected'];
+        foreach ($orders as $order) {
+            $index = null;
+            if (in_array($order->status, $status)) {
+                $index = array_search($order->status, $status);
+
+                if ($index === 4) {
+                    $user = $users->get(fake()->numberBetween(0, $users_count -1));
+                    TransactionContoller::add($order->id, $user->id, $status[fake()->numberBetween(0,1)], $status[$index]);
+                } else {
+                    for ($i = 1; $i < $index + 1; $i++) {
+                        $user = $users->get(fake()->numberBetween(0, $users_count -1));
+                        TransactionContoller::add($order->id, $user->id, $status[$i-1], $status[$i]);
+                    }
+                }
+            }
+        }
     }
 }
